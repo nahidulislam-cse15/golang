33be41b0b3e7f250cab0f88a6b1f40ad305908b2
Md. Nahidul Islam <nahidulss@gmail.com>
2022-03-23 10:52:44 +0600

    header set

--- web/main.go
+++ web/main.go
@@ -6,26 +6,29 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Wlcome to Master Academy</h1>")
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Println(w.Header().Get("Content-Type"))
+	fmt.Fprint(w, "<h1>Welcome to Master Academy</h1>")
 }
-func about(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Wlcome to Master Academy about page</h1>")
-}
-func contact(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Wlcome to Master Academy contact page</h1>")
-}
-func blog(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Wlcome to Master Academy blog page</h1>")
-}
+
+// func about(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type:", "text/plain")
+// 	fmt.Fprint(w, "<h1>Welcome to Master Academy about page</h1>")
+// }
+// func contact(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "<h1>Welcome to Master Academy contact page</h1>")
+// }
+// func blog(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "<h1>Wlcome to Master Academy blog page</h1>")
+// }
 
 func main() {
 	http.HandleFunc("/", home)
-	http.HandleFunc("/about", about)
-	http.HandleFunc("/contact", contact)
-	http.HandleFunc("/blog", blog)
-	
+	// http.HandleFunc("/about", about)
+	// http.HandleFunc("/contact", contact)
+	// http.HandleFunc("/blog", blog)
 
 	fmt.Println("Welcome")
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8080", nil)
 
 }
