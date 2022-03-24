8d46a844d2c80d16f21fb6cbb335ee406ed6f47b
Md. Nahidul Islam <nahidulss@gmail.com>
2022-03-25 00:08:55 +0600

    http handle handler type conversion added

--- web/main.go
+++ web/main.go
@@ -12,33 +12,33 @@ func home(w http.ResponseWriter, r *http.Request) {
 	//http.StatusNotFound
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
+func pathHandler(w http.ResponseWriter, r *http.Request) {
 
-// 	fmt.Fprintln(w, r.URL.Path)//deoceded path
-// 	fmt.Fprintln(w, r.URL.RawPath)//encoded path
-// 	switch r.URL.Path{
-// 	 case "/":
-// 		home(w,r)
-// 	case "/about":
-// 		about(w,r)	
-// 	case "/contact":
-// 		contact(w,r)
-// 	case "/blog":
-// 		blog(w,r)	
-// 	default:
-// 		//way 1
-// 		// w.WriteHeader(http.StatusNotFound)
-// 		// fmt.Fprint(w,"Page not found")
+	fmt.Fprintln(w, r.URL.Path)//deoceded path
+	fmt.Fprintln(w, r.URL.RawPath)//encoded path
+	switch r.URL.Path{
+	 case "/":
+		home(w,r)
+	case "/about":
+		about(w,r)	
+	case "/contact":
+		contact(w,r)
+	case "/blog":
+		blog(w,r)	
+	default:
+		//way 1
+		// w.WriteHeader(http.StatusNotFound)
+		// fmt.Fprint(w,"Page not found")
 
-// 		//way 2
-// 		http.Error(w,"Page not found",http.StatusNotFound)
-// 		//way 3
-// 		http.Error(w,http.StatusText(http.StatusNotFound),http.StatusNotFound)
+		//way 2
+		http.Error(w,"Page not found",http.StatusNotFound)
+		//way 3
+		http.Error(w,http.StatusText(http.StatusNotFound),http.StatusNotFound)
 
 
-// 	}
+	}
 
-// }
+}
 
 
 func about(w http.ResponseWriter, r *http.Request) {
@@ -77,14 +77,19 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request){
 }
 func main() {
 
-	http.HandleFunc("/", pathHandler)
+	//http.HandleFunc("/", pathHandler)
 	// http.HandleFunc("/about", about)
 	// http.HandleFunc("/contact", contact)
 	// http.HandleFunc("/blog", blog)
+	//http.Handle("/",http.HandlerFunc(pathHandler))
+	//http.HandleFunc("/",http.HandlerFunc(pathHandler).ServeHTTP)//default servemux
 
 	fmt.Println("Welcome")
 	//http.ListenAndServe(":8090", nil)
-	var router Router
-	http.ListenAndServe(":8090", router)
+	// var router http.HandlerFunc
+	// router=pathHandler
+	//http.ListenAndServe(":8090", router)
+	http.ListenAndServe(":8090", http.HandlerFunc(pathHandler))//pathhandler func converted to handler type
+
 
 }
