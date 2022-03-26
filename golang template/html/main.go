d115eb7590f42798e076653942eecfa98d80f6fe
Md. Nahidul Islam <nahidulss@gmail.com>
2022-03-26 12:11:47 +0600

    ExecuteTemplate added

--- golang template/html/main.go
+++ golang template/html/main.go
@@ -9,13 +9,14 @@ import (
 
 func main() {
 
-template.
+
 	temp, err:= template.ParseFiles("one.gohtml","two.gohtml") //return first files
 	if err != nil {
 		fmt.Println(err.Error)
 	}
 	err = temp.Execute(os.Stdout, "nahid") //2nd param may b nil or any type received by {{.}} (pipelining)
-
+     
+	err=temp.ExecuteTemplate(os.Stdout,"two.gohtml",nil)
 
 }
 
