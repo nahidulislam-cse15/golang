f7412c6dc8fb23f78f495d620a98aaf4c894e43f
Md. Nahidul Islam <nahidulss@gmail.com>
2022-03-26 12:17:20 +0600

    parseGlob added

--- golang template/html/main.go
+++ golang template/html/main.go
@@ -10,13 +10,14 @@ import (
 func main() {
 
 
-	temp, err:= template.ParseFiles("one.gohtml","two.gohtml") //return first files
+	//temp, err:= template.ParseFiles("one.gohtml","two.gohtml") //return first files
+	temp,err := template.ParseGlob("*.gohtml")//increasing order filename selected
 	if err != nil {
 		fmt.Println(err.Error)
 	}
 	err = temp.Execute(os.Stdout, "nahid") //2nd param may b nil or any type received by {{.}} (pipelining)
      
-	err=temp.ExecuteTemplate(os.Stdout,"two.gohtml",nil)
+//	err=temp.ExecuteTemplate(os.Stdout,"two.gohtml",nil)
 
 }
 
