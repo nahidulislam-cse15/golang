865a89518eeecc8f9638899b72fd54664bd72887
Md. Nahidul Islam <nahidulss@gmail.com>
2022-03-26 22:53:01 +0600

    data passing via pipeline and iterate template by range

--- golang template/html/main.go
+++ golang template/html/main.go
@@ -15,7 +15,9 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error)
 	}
-	err = temp.Execute(os.Stdout, "nahid") //2nd param may b nil or any type received by {{.}} (pipelining)
+	//data pass
+	names:=[]string{"nahid","tasin","tasrif","raiyan"}
+	err = temp.Execute(os.Stdout, names) //2nd param may b nil or any type received by {{.}} (pipelining)
      
 //	err=temp.ExecuteTemplate(os.Stdout,"two.gohtml",nil)
 
